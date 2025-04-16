3a9aa23e32b418848422d9da238b5a61723dc148
Thomas <thomas.chausse.1@ens.etsmtl.ca>
2025-04-16 14:51:46 -0400

    cmake cross compile multiplatform wip

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,27 +1,34 @@
 set(CPU "" CACHE STRING "Target CPU architecture")
-# cmake -B build -DCMAKE_TOOLCHAIN_FILE=toolchain-arm-none-eabi.cmake -DCPU=cortex-m7
 
-# # STM32F401xe toolchain (arm-none-eabi)
+# STM32F401xe toolchain (arm-none-eabi)
 set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_SYSTEM_PROCESSOR arm)
+# set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_SYSTEM_PROCESSOR cortex-m4)
+# Disable test programs
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+set(CMAKE_C_COMPILER_WORKS TRUE)
+set(CMAKE_CXX_COMPILER_WORKS TRUE)
+
+# Librairies
+set(CURRENT_PLATFORM "platform/stmf401xe")
 
 # Compilers
-set(CMAKE_C_COMPILER arm-none-eabi-gcc)
-set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
-set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
+set(CMAKE_C_COMPILER /usr/bin/arm-none-eabi-gcc)
+set(CMAKE_ASM_COMPILER /usr/bin/arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/arm-none-eabi-g++)
 
-set(CPU_FLAGS "-mcpu=${CPU} -mthumb")
-set(CMAKE_C_FLAGS "${CPU_FLAGS} -ffreestanding -Wall")
-set(CMAKE_CXX_FLAGS "${CPU_FLAGS} -fno-rtti -fno-exceptions -ffreestanding -Wall")
-set(CMAKE_ASM_FLAGS "${CPU_FLAGS}")
+# # Ensure CMake knows how to compile objects
+set(CMAKE_C_COMPILE_OBJECT "<CMAKE_C_COMPILER> <FLAGS> -o <OBJECT> -c <SOURCE>")
+set(CMAKE_C_CREATE_STATIC_LIBRARY "<CMAKE_AR> cr <TARGET> <OBJECTS>")
 
-option(BUILD_BOOTLOADER "Build bootloader" ON)
-option(BUILD_APP "Build main app" ON)
+# Add CPU flags and common options
+add_compile_options(
+    -mcpu=cortex-m4
+    -mthumb
+    -ffreestanding
+    -Wall
+)
 
-if(BUILD_BOOTLOADER)
-  add_subdirectory(bootloader)
-endif()
-
-if(BUILD_APP)
-  add_subdirectory(app)
-endif()
+# Optional: set C++ specific flags
+add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-fno-rtti>)
+add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-fno-exceptions>)
